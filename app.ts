7abb9c1bbdfae04d2e0051c6a7d8805da17ab8e9
72xarth <pikachu0940188505@gmail.com>
2024-03-04 00:16:40 +0700

    LOGIN Success

--- app.ts
+++ app.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { router as game } from "./api/game";
+import cors from "cors";
 export const app = express();
+
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
 app.use(bodyParser.json());
 app.use("/game",game);
